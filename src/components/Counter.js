e2c2ef7a35db0dd7e42a38ab811e2ed7aeeea3b9
hana-nazer <hanapp9966@gmail.com>
2023-01-26 00:47:16 +0530

    hide and unhide counter value

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,6 +4,7 @@ import { useSelector,useDispatch } from 'react-redux';
 const Counter = () => {
   const dispatch = useDispatch()
   const counter = useSelector(state =>state.counter)
+  const show = useSelector(state=>state.showCounter)
 const incrementHandler =()=>{
   dispatch({type:'increment'})
 }
@@ -11,14 +12,22 @@ const decrementHandler =()=>{
   dispatch({type:'decrement'})
 }
 
-  const toggleCounterHandler = () => {};
+const increaseHandler =()=>{
+  dispatch({type:'increase', amount:5})
+}
+
+  const toggleCounterHandler = () => {
+    dispatch({type:'toggle'})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div> }
       <div>
         <button onClick={incrementHandler}>increment</button>
+        <button onClick={increaseHandler}>increment by 5</button>
+
         <button onClick={decrementHandler}>decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
