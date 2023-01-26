baa797270a610fad3749d2d79af5ed664efd82c0
hana-nazer <hanapp9966@gmail.com>
2023-01-26 10:37:12 +0530

    working with multiple slices

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,30 +1,30 @@
-import classes from './Counter.module.css';
-import { useSelector,useDispatch } from 'react-redux';
-import {conuterActions } from '../store/index'
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
+import { conuterActions } from "../store/index";
 
 const Counter = () => {
-  const dispatch = useDispatch()
-  const counter = useSelector(state =>state.counter)
-  const show = useSelector(state=>state.showCounter)
-const incrementHandler =()=>{
-  dispatch(conuterActions.increment())
-}
-const decrementHandler =()=>{
-  dispatch(conuterActions.decrement())
-}
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
+  const incrementHandler = () => {
+    dispatch(conuterActions.increment());
+  };
+  const decrementHandler = () => {
+    dispatch(conuterActions.decrement());
+  };
 
-const increaseHandler =()=>{
-  dispatch(conuterActions.increase(5))
-}
+  const increaseHandler = () => {
+    dispatch(conuterActions.increase(5));
+  };
 
   const toggleCounterHandler = () => {
-    dispatch(conuterActions.toggleCounter())
+    dispatch(conuterActions.toggleCounter());
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      {show && <div className={classes.value}>{counter}</div> }
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>increment</button>
         <button onClick={increaseHandler}>increment by 5</button>
